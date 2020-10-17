c956edaa98bcffce96d6c1c33454b208e31571ce
Dawid Janczak <dawid.janczak@gmail.com>
2020-10-17 15:37:13 +0800

    Adapt purge to not clear deeply nested HTML

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 module.exports = {
   future: {
-    // removeDeprecatedGapUtilities: true,
+    removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true
   },
   purge: [
-    '**/*.html'
+    '_drafts/*.html',
+    '_layouts/*.html',
+    '_posts/*.html',
+    '*.html'
   ],
   theme: {
     extend: {},
