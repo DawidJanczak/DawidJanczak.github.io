462e442079a3d155ae80d036463a01f1ca8f056d
Dawid Janczak <dawid.janczak@gmail.com>
2020-11-06 11:53:36 +0800

    Finish up with styling the Top 100 and fix colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,9 +13,12 @@ module.exports = {
     '*.html'
   ],
   theme: {
-    colors: {
-      accent: '#E6F2F2',
-      'top-100-bg': '#4F6E83'
+    extend: {
+      colors: {
+        accent: '#E6F2F2',
+        'top-100-bg': '#4F6E83',
+        'top-100-text': '#B6D1E4'
+      }
     }
   },
   variants: {},
